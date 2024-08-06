4ed56fa2291f8c77ee580816f45476f430176ad6
Tjcarroll1 <tjcarroll1@icloud.com>
2024-08-06 21:36:46 +1000

    fixing animated circle

--- src/app/components/home/AnimatedCircle.tsx
+++ src/app/components/home/AnimatedCircle.tsx
@@ -1,4 +1,4 @@
-// src/components/AnimatedCircle.tsx
+'use client';
 import React from 'react';
 import { motion } from 'framer-motion';
 import { getTailwindColor } from '../../../utils/tailwindColors';
