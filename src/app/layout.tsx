96ab1d304ab81bd9b6da7373e9b620fca5d3373b
Tjcarroll1 <tjcarroll1@icloud.com>
2024-09-14 05:50:47 +1000

    added chatbot and facebook widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,11 +60,44 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Facebook Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '830105865970257');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* End Facebook Pixel Code */}
+
+        <script async defer src="https://app.primechat.ai/js/widget/laxvljenwhsjcjyn/float.js"></script>
       </head>
       <body className="flex min-h-screen flex-col font-sans">
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
+
+        {/* Facebook Pixel Noscript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=830105865970257&ev=PageView&noscript=1"
+            alt="fb-pixel"
+          />
+        </noscript>
+        {/* End Facebook Pixel Noscript */}
       </body>
     </html>
   );
