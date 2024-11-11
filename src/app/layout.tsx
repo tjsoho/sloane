98413e7b62925cd3a3232be39b51ee0941f39a3b
Tjcarroll1 <tjcarroll1@icloud.com>
2024-11-11 21:21:23 +1000

    pixel code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,7 +77,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '830105865970257');
+              fbq('init', '1736192697203766');
               fbq('track', 'PageView');
             `,
           }}
@@ -101,7 +101,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=830105865970257&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1736192697203766&ev=PageView&noscript=1"
             alt="fb-pixel"
           />
         </noscript>
