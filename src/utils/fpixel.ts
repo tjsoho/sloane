12c97f5ac6ebe459c64de1b016bcf45b657530e0
Tjcarroll1 <tjcarroll1@icloud.com>
2024-11-12 11:06:51 +1000

    adding domain for fb pixel

--- src/utils/fpixel.ts
+++ src/utils/fpixel.ts
@@ -1,10 +1,26 @@
 export const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID
 
+// Remove development check and just set domain
+const domain = 'sloane.biz'
+
 export const pageview = () => {
-  window.fbq('track', 'PageView')
+  if (typeof window !== 'undefined' && window.location.hostname === domain) {
+    window.fbq('track', 'PageView', {
+      source_url: window.location.href,
+      host_name: window.location.hostname,
+      domain: domain
+    });
+  }
 }
 
 // For tracking specific events
 export const event = (name: string, options = {}) => {
-  window.fbq('track', name, options)
+  if (typeof window !== 'undefined' && window.location.hostname === domain) {
+    window.fbq('track', name, {
+      ...options,
+      source_url: window.location.href,
+      host_name: window.location.hostname,
+      domain: domain
+    });
+  }
 } 
