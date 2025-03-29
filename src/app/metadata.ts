89ccf625e239c3e9d1cd7dbef18796cfda4515e3
Tjcarroll1 <tjcarroll1@icloud.com>
2025-03-29 15:19:42 +1000

    added meta stuff

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,9 +1,32 @@
-export const metadata = {
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
     title: 'Sloane',
-    description: 'Making business easy',
-    icons: [{ rel: 'icon', url: '/favicon.ico' }],
+    description: 'Work Smart. Live Well.',
+    icons: {
+      icon: '/favicon.ico',
+    },
     openGraph: {
-      images: [],
+      title: 'Sloane - Work Smart. Live Well.',
+      description: 'The AI tool that helps business owners grow and while living well and staying in touch with their true selves.',
+      url: 'https://sloane.biz',
+      siteName: 'Sloane',
+      images: [
+        {
+          url: 'https://sloane.biz/_next/image/?url=%2Fimages%2FheroMOck.png&w=828&q=75', // This should be your main preview image
+          width: 1200,
+          height: 630,
+          alt: 'Sloane Webapp',
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: 'Sloane - Making Business Easy',
+      description: 'The AI tool that helps business owners grow and thrive',
+      images: ['https://sloane.biz/images/og-image.png'],
     },
   };
   
